32973c0b8677177d29de3a5874bc07ced9b5f2b6
MatteoDebart <m4tteo.debart@gmail.com>
2025-01-22 15:20:54 +0100

    add dinov2 vectorizer

---
+++ image_preprocessing/dino_preprocessing.py
@@ -0,0 +1,10 @@
+from image_feature_extraction import ImagePreprocessingFunction
+import numpy as np
+from PIL import Image
+
+class ImageSegmentationPreprocessor(ImagePreprocessingFunction):
+    def __init__(self):
+        super.__init__(self.preprocess_image)
+
+    def preprocess_image():
+        return
